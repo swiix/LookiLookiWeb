d00669e97705e2a417f49b979c656e492fa8d5a2
Dawid Paliczuk <github@paliczuk.de>
2025-11-27 15:02:13 +0700

    Simplify server config - listen on 0.0.0.0

--- server.ts
+++ server.ts
@@ -4,10 +4,8 @@ import next from "next";
 import { Server } from "socket.io";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = process.env.HOSTNAME || "0.0.0.0";
 const port = parseInt(process.env.PORT || "3000", 10);
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -60,7 +58,7 @@ app.prepare().then(() => {
             console.error(err);
             process.exit(1);
         })
-        .listen(port, hostname, () => {
-            console.log(`> Ready on http://${hostname}:${port}`);
+        .listen(port, "0.0.0.0", () => {
+            console.log(`> Ready on http://0.0.0.0:${port}`);
         });
 });
